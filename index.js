cd20733bc23897b4bad0dd18193dc60ca59cda39
Nkosi Enciso Givhan <koasterman@gmail.com>
2018-03-06 22:31:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var recipes = new Object ({})
 function updateObjectWithKeyAndValue(obj, key, value){
-  
+  obj[key] = value
+  return obj
 }
 function deleteFromObjectByKey(obj, key){
   return obj
